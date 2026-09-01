4822a0f13aa3a395a5ed11ff0afdeef35254aa7a
ivanshkurko7-arch <ivanshkurko7@gmail.com>
2026-09-01 22:26:27 +0300

    Update transactions.js

--- netlify/functions/transactions.js
+++ netlify/functions/transactions.js
@@ -4,7 +4,7 @@ const { notifyFamily } = require('./lib/notify');
 
 const CORS = {
   'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*',
+     'Access-Control-Allow-Origin': 'https://wa1et.netlify.app',
 };
 
 function fmt(n) {
